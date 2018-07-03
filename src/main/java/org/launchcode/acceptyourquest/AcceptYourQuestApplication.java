d87e8b12b5296fb6a0812fc55496b75d38654cb5
deusknight <deusknight@gmail.com>
2018-07-03 16:33:06 -0500

    Initialized

---
+++ src/main/java/org/launchcode/acceptyourquest/AcceptYourQuestApplication.java
@@ -0,0 +1,12 @@
+package org.launchcode.acceptyourquest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcceptYourQuestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AcceptYourQuestApplication.class, args);
+	}
+}
---
+++ src/test/java/org/launchcode/acceptyourquest/AcceptYourQuestApplicationTests.java
@@ -0,0 +1,16 @@
+package org.launchcode.acceptyourquest;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AcceptYourQuestApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
